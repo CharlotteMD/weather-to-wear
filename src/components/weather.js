9227074b9c050a4f6ebdc5f2abcd7e4a0cdeb3de
CharlotteMD <charlottemdavies@hotmail.co.uk>
2018-12-11 16:51:27 +0000

    starting to add some logic

--- src/components/weather.js
+++ src/components/weather.js
@@ -4,6 +4,15 @@ import '../App.css';
 import Axios from 'axios';
 
 class Weather extends Component {
+  constructor() {
+    super();
+    this.state = {
+      feelstemp: '',
+      actualtemp: '',
+      wind: '',
+      rain: ''
+    };
+  }
 
 
   componentDidMount() {
@@ -12,11 +21,22 @@ class Weather extends Component {
     var key = '63c7e1fe04debd05e2a196e39bc9e9c4';
 
     Axios
-      .get(`https://api.darksky.net/forecast/${key}/${latitude},${longitude}`)
+      .get(`https://api.darksky.net/forecast/${key}/${latitude},${longitude}?units=si`)
       .then((response) => {
         console.log(response);
-        var currentweather = response.data.currently.summary;
-        console.log('weather ' + currentweather);
+
+        var feelstemp = response.data.currently.apparentTemperature;
+        var actualtemp = response.data.currently.temperature;
+        var wind = response.data.currently.windSpeed;
+        var rain = response.data.minutely.data[0].precipProbability;
+
+        this.setState({ 
+          feelstemp: feelstemp,
+          actualtemp: actualtemp,
+          wind: wind,
+          rain: rain
+        })
+
       })
       .catch(err => console.log(err));
   }
@@ -26,6 +46,19 @@ class Weather extends Component {
       <div className="Weather">
         <h2>What should I wear today?</h2>
         <div>
+
+          <p>What kind of coat do I need? Should I take an umbrella? What kind of shoes should I wear? Do I need a jumper? Do I need a hat and scarf?</p>
+
+          <p>It is currently {this.state.actualtemp}, although it feels like {this.state.feelstemp}!</p>
+
+
+        {this.state.wind >= 5 &&
+          <p>It's a bit windy outside! Perhaps take an extra jumper and don't wear a hat!</p>}
+
+        { this.state.wind < 5 &&
+          <p>There is a light breeze.</p>}
+
+          <p>And there is a {this.state.rain} chance it's already raining!</p>
 
         </div>
 
